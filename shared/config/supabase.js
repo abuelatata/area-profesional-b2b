f286200a7a05235af383aa151e38be5818207448
abuelatata <artesaniaabuelatata@gmail.com>
2026-03-29 15:30:58 +0200

    Update Supabase anon key in configuration

--- shared/config/supabase.js
+++ shared/config/supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://qoyrjhgjtydbcngfnyqn.supabase.co";
-const SUPABASE_ANON_KEY = "PEGA_AQUI_TU_ANON_KEY";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFveXJqaGdqdHlkYmNuZ2ZueXFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5MTQ2NzQsImV4cCI6MjA4ODQ5MDY3NH0.ASOVIDOiJcuWOWExEzGBvwjjKEpZIfRLbxgVyc6Xr64";";
 
 export const supabase = window.supabase.createClient(
   SUPABASE_URL,
